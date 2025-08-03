2a7f46d5900daebc009ebe1b92512adf7067af62
magg530m <magg530m@gmail.com>
2025-08-03 05:42:24 +0000

    0803commit2

--- Lesson9/lesson9_1.sql
+++ Lesson9/lesson9_1.sql
@@ -24,20 +24,34 @@ GROUP BY "name"
 *基隆火車站2020,2021,2022,每年進站人數
 */
 
-SELECT *
-FROM "每日各站進出站人數" LEFT JOIN "台鐵車站資訊" ON "車站代碼" = "stationCode"
-WHERE "基隆" BETWEEN '2020-01-01' AND '2022-12-31'
-GROUP BY "name"
+SELECT 
+    EXTRACT(YEAR FROM "日期"::date) AS 年份,
+    SUM("進站人數") AS 年度進站人數總計
+FROM "每日各站進出站人數" p
+JOIN "台鐵車站資訊" s ON p."車站代碼" = s."stationCode"
+WHERE s."stationName" = '基隆'
+    AND "日期"::date >= '2020-01-01'
+    AND "日期"::date <= '2022-12-31'
+GROUP BY EXTRACT(YEAR FROM "日期"::date)
+ORDER BY 年份;
 
 /*
 *基隆火車站, 台北火車站,2020,2021,2022,每年進站人數
 */
 
-SELECT *
-FROM "每日各站進出站人數" LEFT JOIN "台鐵車站資訊" ON "車站代碼" = "stationCode"
-WHERE "基隆" BETWEEN '2020-01-01' AND '2022-12-31'
-GROUP BY "name"
-
+SELECT 
+    CASE 
+        WHEN "車站代碼" = 900 THEN '基隆'
+        WHEN "車站代碼" = 1000 THEN '臺北'
+    END AS 車站名稱,
+    EXTRACT(YEAR FROM "日期"::date) AS 年份,
+    SUM("進站人數") AS 年度進站人數總計
+FROM "每日各站進出站人數"
+WHERE "車站代碼" IN (900, 1000)
+    AND "日期"::date >= '2020-01-01'
+    AND "日期"::date <= '2022-12-31'
+GROUP BY "車站代碼", EXTRACT(YEAR FROM "日期"::date)
+ORDER BY "車站代碼", 年份;
 /*
 *查詢2022年平均每日進站人數超過2萬人的站點
 */
